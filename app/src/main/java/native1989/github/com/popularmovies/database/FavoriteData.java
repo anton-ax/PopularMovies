14191435bde8df338df1fad6ea5abddfd9c3a4fe
Anton <native1989@gmail.com>
2015-07-05 03:11:30 +0400

    Clean code

--- app/src/main/java/native1989/github/com/popularmovies/database/FavoriteData.java
+++ app/src/main/java/native1989/github/com/popularmovies/database/FavoriteData.java
@@ -1,14 +1,9 @@
 package native1989.github.com.popularmovies.database;
 
-import android.content.ContentValues;
 import android.content.Context;
-import android.database.Cursor;
-import android.database.DatabaseUtils;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
-
-import native1989.github.com.popularmovies.model.Movie;
 
 /**
  * Created by Anton on 7/3/2015.
@@ -20,7 +15,6 @@ public class FavoriteData extends SQLiteOpenHelper {
 
     private static final String TABLE_NAME = "favorite";
     private final static String COLUMN_ID = "_id";
-    private final static String ID = "id";
 
     private static final String DATABASE_CREATE = "create table "
             + TABLE_NAME + "(" + COLUMN_ID
@@ -51,35 +45,4 @@ public class FavoriteData extends SQLiteOpenHelper {
         onCreate(db);
     }
 
-    public void add(Movie movie) {
-        SQLiteDatabase sqLiteDatabase = this.getWritableDatabase();
-        ContentValues values = new ContentValues();
-        values.put("id", movie.getId());
-        values.put("poster_path", movie.getPoster_path());
-        values.put("title", movie.getTitle());
-        values.put("vote_average", movie.getVote_average());
-        values.put("overview", movie.getOverview());
-        values.put("backdrop_path", movie.getBackdrop_path());
-        values.put("release_date", movie.getRelease_date());
-        sqLiteDatabase.insert(TABLE_NAME, null, values);
-    }
-
-    public void remove(Movie movie) {
-        SQLiteDatabase sqLiteDatabase = this.getWritableDatabase();
-        sqLiteDatabase.delete(TABLE_NAME, "id=?", new String[]{String.valueOf(movie.getId())});
-    }
-
-    public Cursor list() {
-        SQLiteDatabase sqLiteDatabase = this.getReadableDatabase();
-        String query = "SELECT _id, id, poster_path, title, vote_average," +
-                "overview, backdrop_path, release_date FROM favorite ORDER BY _id";
-        return sqLiteDatabase.rawQuery(query, null);
-    }
-
-    public boolean exists(Integer id) {
-        SQLiteDatabase db = this.getReadableDatabase();
-        long num = DatabaseUtils.queryNumEntries(db, TABLE_NAME, "id=?",
-                new String[]{String.valueOf(id)});
-        return num > 0;
-    }
 }
